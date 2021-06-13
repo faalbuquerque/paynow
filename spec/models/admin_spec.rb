59cee963fe1833cab358a9cca074aa91c1f9d763
Fabiane Albuquerque <bii.nett@gmail.com>
2021-06-12 23:56:54 -0300

    Fix admin manages test

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Manages::AdminsController, type: :controller do
+RSpec.describe Admins::ManagesController, type: :controller do
   it { should use_before_action(:authenticate_admin!) }
-  it { should route(:get, '/manages/admins').to(action: :index) }
+  it { should route(:get, '/admins/manages').to(action: :index) }
 end
 
 RSpec.describe Admin, type: :model do
